96464d9e0ea86af5391628555cd22f855ff0ab66
Simon Jonsson <nohatssir@users.noreply.github.com>
2013-12-15 17:45:07 +0100

    Add beginning of wizard

---
+++ var/www/openmediavault/js/omv/module/admin/service/cups/window/PrinterWizard.js
@@ -0,0 +1,243 @@
+/**uid/
+* Copyright (C) 2013 OpenMediaVault Plugin Developers
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// require("js/omv/Rpc.js")
+// require("js/omv/window/Window.js")
+// require("js/omv/window/MessageBox.js")
+// require("js/omv/module/admin/service/cups/window/Wizard.js")
+
+Ext.define("OMV.module.admin.service.cups.window.PrinterWizard", {
+    extend   : "OMV.module.admin.service.cups.window.Wizard",
+    requires : [
+        "OMV.Rpc"
+    ],
+
+    title  : _("Add printer"),
+    height : 360,
+
+    getCardItems : function() {
+        var me = this;
+
+        return [{
+            xtype : 'panel',
+            items : [{
+                html   : '<h1>' + _('Printer Hardware') + '</h1>',
+                border : false
+            },{
+                border : false,
+                html   : "<p>"
+                       + _("If your printer is not listed here, try Make: Generic, Model: Generic Postscript Printer. If there is more than one driver for your make / model choose one that is labeled (recommended) if available, or experiment to find the driver that works best for you.")
+                       + "</p>"
+            },{
+                xtype  : 'fieldset',
+                layout : 'fit',
+                title  : _("Printer"),
+                border : false,
+                items  : [{
+                    xtype          : 'combo',
+                    name           : 'device',
+                    hideFieldLabel : 'true',
+                    valueField     : 'uuid',
+                    displayField   : 'DeviceInfo',
+                    emptyText      : _("Select a printer ..."),
+                    allowBlank     : false,
+                    allowNone      : false,
+                    editable       : false,
+                    triggerAction  : "all",
+                    store          : Ext.create("OMV.data.Store", {
+                        autoLoad : true,
+                        model    : OMV.data.Model.createImplicit({
+                            idProperty : "uuid",
+                            fields     : [
+                                { name : "uuid" },
+                                { name : "DeviceInfo" }
+                            ]
+                        }),
+                        proxy : {
+                            type : "rpc",
+                            rpcData : {
+                                service : "Cups",
+                                method  : "findDirectPrinters"
+                            },
+                            appendSortParams : false
+                        },
+                        listeners : {
+                            beforeload : function () {
+                                OMV.MessageBox.wait(null, _("Scanning for attached printers ..."));
+                            },
+                            load : function (store, records) {
+
+                                OMV.MessageBox.updateProgress(1);
+                                OMV.MessageBox.hide();
+
+                                if (!records.length) {
+                                    OMV.MessageBox.error(null, _("No printers found."));
+                                    return;
+                                }
+
+                                var makeCombo = me.findField("make").setDisabled(false);
+                            }
+                        }
+                    }),
+                    listeners : {
+                        select : function (combo, record) {
+                            var deviceInfo = record.get("DeviceInfo");
+                            var nameField = me.findField("name");
+                            var descriptionField = me.findField("description");
+
+                            nameField.setValue(mm.replace(/[\s|#|\/]/g, '_'));
+                            descriptionField.setValue(deviceInfo);
+                        }
+                    }
+                }]
+            },{
+                xtype  : 'fieldset',
+                layout : 'fit',
+                title  : _("Make"),
+                border : false,
+                items  : [{
+                    xtype          : 'combo',
+                    name           : 'make',
+                    hideFieldLabel : 'true',
+                    valueField     : 'make',
+                    displayField   : 'make',
+                    submitValue    : false,
+                    emptyText      : _("Select a make ..."),
+                    allowBlank     : false,
+                    allowNone      : false,
+                    editable       : false,
+                    triggerAction  : "all",
+                    disabled       : true,
+                    store          : Ext.create("OMV.data.Store", {
+                        autoLoad : true,
+                        model    : OMV.data.Model.createImplicit({
+                            idProperty : "make",
+                            fields     : [
+                                { name : "make" },
+                                { name : "models" }
+                            ]
+                        }),
+                        proxy : {
+                            type : "rpc",
+                            rpcData : {
+                                service : "Cups",
+                                method  : "getMakesModels"
+                            },
+                            appendSortParams : false
+                        }
+                    }),
+                    listeners : {
+                        select:function (combo, record) {
+                            var modelField = me.findField("model");
+                            var modelStore = modelField.getStore();
+
+                            modelField.setDisabled(false);
+                            modelField.setValue(null);
+                            modelStore.removeAll();
+
+                            Ext.each(record.get('models'), function (model) {
+                                modelStore.add([
+                                    new Ext.data.Record(model)
+                                ]);
+                            });
+                        }
+                    }
+                }]
+            },{
+                xtype  : 'fieldset',
+                layout : 'fit',
+                title  : _("Model / Driver"),
+                border : false,
+                items  : [{
+                    xtype          : 'combo',
+                    name           : 'model',
+                    hideFieldLabel : 'true',
+                    valueField     : 'uuid',
+                    displayField   : 'PpdMakeAndModel',
+                    emptyText      : _("Select a model ..."),
+                    allowBlank     : false,
+                    allowNone      : false,
+                    editable       : false,
+                    triggerAction  : "all",
+                    disabled       : true,
+                    mode           : "local",
+                    autoLoad       : false,
+                    store          : Ext.create("Ext.data.SimpleStore", {
+                        fields : [
+                            { name : "uuid" },
+                            { name : "PpdMakeAndModel" }
+                        ],
+                        data : []
+                    })
+                }]
+            }]
+        },{
+            xtype : 'panel',
+            items : [{
+                html   : '<h1>' + _('Printer Description') + '</h1>',
+                border : false
+            },{
+                xtype    : "fieldset",
+                layout   : 'form',
+                border   : true,
+                defaults : {
+                    anchor : '100%'
+                },
+                items : [{
+                    xtype      : "textfield",
+                    name       : "name",
+                    fieldLabel : _("Name"),
+                    plugins    : [{
+                        ptype : "fieldinfo",
+                        text  : _("May contain any printable characters except \"/\", \"#\", and space")
+                    }],
+                    vtype      : "printername",
+                    allowBlank : false
+                },{
+                    xtype      : "textfield",
+                    name       : "description",
+                    fieldLabel : _("Description"),
+                    plugins    : [{
+                        ptype : "fieldinfo",
+                        text  : _('Human-readable description such as "HP LaserJet with Duplexer"')
+                    }],
+                    allowBlank : false
+                },{
+                    xtype      : "textfield",
+                    name       : "location",
+                    fieldLabel : _("Location"),
+                    plugins    : [{
+                        ptype : "fieldinfo",
+                        text  : _('Human-readable location such as "Lab 1"')
+                    }]
+                }]
+            }]
+        }];
+    }
+});
+
+/**
+ * Printer names validation
+ */
+Ext.apply(Ext.form.VTypes, {
+    printername : function (value) {
+        return /^[^\s|#|\\]+$/.test(value);
+    },
+    printernameText : _("Invalid printer name"),
+    printernameMask : /[^\s|#|\\]/
+});
+
---
+++ var/www/openmediavault/js/omv/module/admin/service/cups/window/Wizard.js
@@ -0,0 +1,216 @@
+/**
+* Copyright (C) 2013 OpenMediaVault Plugin Developers
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// require("js/omv/Rpc.js")
+// require("js/omv/window/Window.js")
+// require("js/omv/window/MessageBox.js")
+
+Ext.define("OMV.module.admin.service.cups.window.Wizard", {
+    extend   : "OMV.window.Window",
+    requires : [
+        "OMV.Rpc"
+    ],
+    uses : [
+        "OMV.window.MessageBox"
+    ],
+
+    title     : _("Wizard"),
+    itemId    : "wizard",
+    layout    : "fit",
+    width     : 500,
+    height    : 320,
+    submitMsg : _("Saving ..."),
+    mode      : "remote",
+
+    defaults : {
+        border : false
+    },
+
+    constructor : function(config) {
+        var me = this;
+
+        me.callParent(arguments);
+        me.addEvents(
+            "beforesubmit",
+            "submit",
+            "exception"
+        );
+    },
+
+    initComponent : function() {
+        var me = this;
+
+        Ext.apply(me, {
+            items : [{
+                xtype    : "form",
+                itemId   : "wizard-form",
+                layout   : "card",
+                defaults : {
+                    border    : false,
+                    bodyStyle : "padding: 10px;",
+                    anchor    : "100%"
+                },
+                bbar  : me.getBbarItems(),
+                items : me.getCardItems()
+            }]
+        });
+
+        me.callParent(arguments);
+    },
+
+    navigate : function(button, direction) {
+        var form   = button.up("#wizard-form");
+        var layout = form.getLayout();
+
+        // Call isValid to validate fields
+        form.isValid();
+        var isValid = true;
+        var fields = layout.activeItem.query("field");
+
+        for (var i = 0, j = fields.length; i < j; i++) {
+            if (!fields[i].isValid()) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (direction === "prev" || isValid)
+            layout[direction]();
+
+        var prev   = form.down("#wizard-prev");
+        var next   = form.down("#wizard-next");
+        var submit = form.down("#wizard-submit");
+
+        prev.setDisabled(!layout.getPrev());
+        next.setDisabled(!layout.getNext());
+
+        if (!layout.getNext()) {
+            next.hide();
+            submit.show();
+        } else {
+            next.show();
+            submit.hide();
+        }
+    },
+
+    getBbarItems : function() {
+        var me = this;
+
+        return [{
+            itemId   : "wizard-prev",
+            text     : _("Previous"),
+            handler  : function(button) {
+                me.navigate(button, "prev");
+            },
+            disabled : true
+        }, "->", {
+            itemId  : "wizard-next",
+            text    : _("Next"),
+            handler : function(button) {
+                me.navigate(button, "next");
+            }
+        },{
+            itemId  : "wizard-submit",
+            text    : _("Finish"),
+            handler : function() {
+                me.onFinishButton();
+            },
+            hidden  : true
+        },{
+            itemId  : "wizard-cancel",
+            text    : _("Cancel"),
+            handler : function() {
+                me.close();
+            }
+        }];
+    },
+
+    getCardItems : function() {
+        return [];
+    },
+
+    getForm : function() {
+        var me = this;
+        return me.down("#wizard-form").getForm();
+    },
+
+    getFormValues : function() {
+        var me = this;
+
+        return me.getForm().getValues();
+    },
+
+    findField : function(id) {
+        var me = this;
+        var basicForm = me.getForm();
+
+        return basicForm.findField(id);
+    },
+
+    onFinishButton : function() {
+        var me = this;
+        var form = me.down("#wizard-form").getForm();
+
+        if (form.isValid()) {
+            me.doSubmit();
+        }
+    },
+
+    doSubmit : function() {
+        var me = this;
+
+        if (me.mode === "remote") {
+            var rpcOptions = {
+                scope       : me,
+                callback    : me.onSubmit,
+                relayErrors : true,
+                rpcData     : {
+                    service : me.rpcService,
+                    method  : me.rpcSetMethod || "set",
+                    params  : me.getFormValues()
+                }
+            };
+
+            if(me.fireEvent("beforesubmit", me, rpcOptions) === false)
+                return;
+
+            // Display waiting dialog.
+            OMV.MessageBox.wait(null, me.submitMsg);
+
+            // Execute RPC.
+            OMV.Rpc.request(rpcOptions);
+        } else {
+            me.fireEvent("submit", me. me.getFormValues());
+            me.close();
+        }
+    },
+
+    onSubmit : function(id, success, response) {
+        var me = this;
+
+        OMV.MessageBox.updateProgress(1);
+        OMV.MessageBox.hide();
+
+        if(success) {
+            me.fireEvent("submit", me, me.getFormValues(), response);
+            me.close();
+        } else {
+            me.fireEvent("exception", me, response);
+            OMV.MessageBox.error(null, response);
+        }
+    }
+});
