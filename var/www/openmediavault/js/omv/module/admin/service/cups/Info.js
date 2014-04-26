7057f2e0634b5ddd3941e77811622667ecb12f33
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-04-26 17:53:08 +0200

    Don't use br tags to do styling
    This reverts commit 04c35c60e76ab697d428935333dc6c4a954d5172.

--- var/www/openmediavault/js/omv/module/admin/service/cups/Info.js
+++ var/www/openmediavault/js/omv/module/admin/service/cups/Info.js
@@ -102,7 +102,8 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 title  : _("Windows"),
                 items  : [{
                     border : false,
-                    html   : "NOTE: The instructions below may vary slightly depending on the version of Windows on which " +
+                    html   : "<p>" +
+                             "NOTE: The instructions below may vary slightly depending on the version of Windows on which " +
                              "the steps are being performed. Some logical deduction may be required on your part." +
                              "</p>" +
                              "<h3>Internet Printing Protocol (IPP)</h3>" +
@@ -153,8 +154,10 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 title  : _("Mac OS X"),
                 items  : [{
                     border : false,
-                    html   : "Shared printers should automatically be available to add in Mac OS X in <b>System Preferences</b> -&gt; <b>Print & Fax</b>. If not, " +
+                    html   : "<p>" +
+                             "Shared printers should automatically be available to add in Mac OS X in <b>System Preferences</b> -&gt; <b>Print & Fax</b>. If not, " +
                              "you can manually add a printer's IPP URL by clicking on <b>IP</b> in the toolbar and selecting the following values:" +
+                             "</p>" +
                              "<ul>" +
                              "<li><b>Protocol:</b> Internet Printing Protocol - IPP</li>" +
                              "<li><b>Address:</b> " + location.hostname + "</li>" +
@@ -171,9 +174,10 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 title  : _("Linux"),
                 items  : [{
                     border : false,
-                    html   : "Consult your window environment's (Gnome, KDE, etc..) or distribution's documentation on adding a shared printer. Printers shared by this system are discoverable and should " +
+                    html   : "<p>" +
+                             "Consult your window environment's (Gnome, KDE, etc..) or distribution's documentation on adding a shared printer. Printers shared by this system are discoverable and should " +
                              "appear when Linux searches for networked printers." +
-                             "<br></br>"
+                             "</p>"
                 }]
             },{
                 // iOS sharing info
@@ -182,8 +186,9 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 title  : _("AirPrint capable Apple iOS devices"),
                 items  : [{
                     border : false,
-                    html   : "If you have enabled AirPrint support, shared printers should be available in the print dialog of the device." +
-                             "<br></br>"
+                    html   : "<p>" +
+                             "If you have enabled AirPrint support, shared printers should be available in the print dialog of the device." +
+                             "</p>"
                }]
             }]
         }];
