a0302a63725062f59d4777c2532f8b3e3b19b0cb
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-05-23 00:14:50 +0200

    update debian files

--- var/www/openmediavault/js/omv/module/admin/service/cups/Info.js
+++ var/www/openmediavault/js/omv/module/admin/service/cups/Info.js
@@ -104,27 +104,35 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 bodyStyle: 'padding-top: ' + (Ext.isIE ? '0' : '10px'),
                 items  : [{
                     border : false,
-                    html   : "NOTE: The instructions below may vary slightly depending on the version of Windows on which " +
+                    html   : "<p>" +
+                             "NOTE: The instructions below may vary slightly depending on the version of Windows on which " +
                              "the steps are being performed. Some logical deduction may be required on your part." +
+                             "</p>" +
                              "<h3>Internet Printing Protocol (IPP)</h3>" +
+                             "<p>" +
                              "Printers are shared to Windows via the IPP URL <b>http://" + location.hostname + ":631/printers/<i>PRINTER_NAME</i>.</b> " +
                              "To add an IPP shared printer in Window\"s <b>Control Panel</b> click on <b>Devices and Printers</b> (or Printers and Faxes), click on <b>Add a Printer</b> " +
                              "and choose to <b>Add a Network Printer</b>, click on " +
                              "<b>The printer that I want isn't listed</b> (if a button with that label is presented), then select the option " +
                              "labeled <b>Select a shared printer by name</b>. In the text box, you may enter the printer's <b>IPP</b> URL as listed above." +
+                             "</p>" +
                              "<h3>SMB</h3>" +
                              "<p>" +
                              "If you have enabled printer sharing over SMB, printers may also be shared to Windows via the SMB path <b>\\\\" + location.hostname + "\\<i>PRINTER_NAME</i></b>" +
                              "</p>" +
+                             "<p>" +
                              "To add an SMB shared printer in Window's <b>Control Panel</b> click on <b>Devices and Printers</b> (or Printers and Faxes), click on <b>Add a Printer</b> " +
                              "and choose to <B>Add a Network Printer</b>, select  <b>Browse for printer</b> (if a button with that label is presented) and select the printer " +
                              "you would like to add. This may prompt you to select or install the printer's driver. After you have connected to a shared printer on the network, " +
                              "you can use it as if it were attached to your computer. " +
+                             "</p>" +
                              "<h3>Point and Print</h3>" +
+                             "<p>" +
                              "Drivers can be made available for Windows clients over SMB to enable " +
                              "'point-and-print' functionality. When a Windows client initially connects to a printer, " +
                              "if a driver is available, it will automatically be downloaded and installed. To manage the SMB drivers installed on this server, follow these steps " +
                              "on a computer running <b>Windows 7 or higher</b>:" +
+                             "</p>" +
                              "<ol style='list-style: decimal; list-style-image: none; margin-left: 10px;'>" +
                              "<li>Click&nbsp;<b>Start</b>, type <b>\\\\" + location.hostname + "</b>, and press Enter. - If prompted to log in, enter the " +
                              "user name and password of an OpenMediaVault account that is part of the <b>lpadmin</b> gruop. This will ensure that " +
@@ -137,7 +145,9 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                              "with the printer.</li>" +
                              "<li>Click <b>Finish </b>to complete the wizard.</li>" +
                              "</ol>" +
-                             "Subsequent attempts to connect to the printer from a Windows computer should automatically install the appropriate printer driver."
+                             "<p>" +
+                             "Subsequent attempts to connect to the printer from a Windows computer should automatically install the appropriate printer driver." +
+                             "</p>"
                 }]
             },{
                 // OS X sharing info
@@ -148,14 +158,18 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 bodyStyle: 'padding-top: ' + (Ext.isIE ? '0' : '10px'),
                 items  : [{
                     border : false,
-                    html   : "Shared printers should automatically be available to add in Mac OS X in <b>System Preferences</b> -&gt; <b>Print & Fax</b>. If not, " +
+                    html   : "<p>" +
+                             "Shared printers should automatically be available to add in Mac OS X in <b>System Preferences</b> -&gt; <b>Print & Fax</b>. If not, " +
                              "you can manually add a printer's IPP URL by clicking on <b>IP</b> in the toolbar and selecting the following values:" +
+                             "</p>" +
                              "<ul>" +
                              "<li><b>Protocol:</b> Internet Printing Protocol - IPP</li>" +
                              "<li><b>Address:</b> " + location.hostname + "</li>" +
                              "<li><b>Queue:</b> printers/PRINTER_NAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(be sure to replace <i>PRINTER_NAME</i> with actual name of printer)</li>" +
                              "</ul>" +
-                             "If you have enabled printer sharing over SMB, the printer may also be available in the Windows section of the <b>Print & Fax</b> dialog."
+                             "<p>" +
+                             "If you have enabled printer sharing over SMB, the printer may also be available in the Windows section of the <b>Print & Fax</b> dialog." +
+                             "</p>"
                 }]
             },{
                 // Linux sharing info
@@ -166,8 +180,10 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 bodyStyle: 'padding-top: ' + (Ext.isIE ? '0' : '10px'),
                 items  : [{
                     border : false,
-                    html   : "Consult your window environment's (Gnome, KDE, etc..) or distribution's documentation on adding a shared printer. Printers shared by this system are discoverable and should " +
-                             "appear when Linux searches for networked printers."
+                    html   : "<p>" +
+                             "Consult your window environment's (Gnome, KDE, etc..) or distribution's documentation on adding a shared printer. Printers shared by this system are discoverable and should " +
+                             "appear when Linux searches for networked printers." +
+                             "</p>"
                 }]
             },{
                 // iOS sharing info
@@ -178,7 +194,9 @@ Ext.define("OMV.module.admin.service.cups.Info", {
                 bodyStyle: 'padding-top: ' + (Ext.isIE ? '0' : '10px'),
                 items  : [{
                     border : false,
-                    html   : "If you have enabled AirPrint support, shared printers should be available in the print dialog of the device."
+                    html   : "<p>" +
+                             "If you have enabled AirPrint support, shared printers should be available in the print dialog of the device." +
+                             "</p>"
                }]
             }]
         }];
