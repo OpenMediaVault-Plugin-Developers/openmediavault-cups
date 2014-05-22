bdd16d13f7302ee44c04f03f0e9253dbc4834a3c
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-05-22 23:33:43 +0200

    update plugin to r1175 changes

--- var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/CupsAccess.js
+++ var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/CupsAccess.js
@@ -21,50 +21,45 @@
 // require("js/omv/util/Format.js")
 
 Ext.define("OMV.module.admin.diagnostic.log.plugin.Cups", {
-	extend: "OMV.module.admin.diagnostic.log.plugin.Plugin",
+    extend : "OMV.module.admin.diagnostic.log.plugin.Plugin",
+    alias  : "omv.plugin.diagnostic.log.cups.access",
 
-	id       : "cupsaccess",
-	text     : _("Printing Access Log"),
-	stateful : true,
-	stateId  : "053eba40-6374-11e3-949a-0800200c9a66",
-	columns  : [{
-		text      : _("Date & Time"),
-		sortable  : true,
-		dataIndex : "date",
-		stateId   : "date",
-		renderer  : OMV.util.Format.localeTimeRenderer()
-	},{
-		text      : _("Host"),
-		sortable  : true,
-		dataIndex : "host",
-		stateId   : "host",
-		flex      : 1
-	},{
-		text      : _("User"),
-		sortable  : true,
-		dataIndex : "user",
-		stateId   : "user",
-		flex      : 1
-	},{
-		text      : _("Event"),
-		sortable  : true,
-		dataIndex : "event",
-		stateId   : "event",
-		flex      : 1
-	}],
-	rpcParams : {
-		id : "cupsaccess"
-	},
-	rpcFields : [
-		{ name : "date", type : "string" },
-		{ name : "host", type : "string" },
-		{ name : "user", type : "string" },
-		{ name : "event", type : "string" }
-	]
-});
-
-OMV.PluginManager.register({
-	ptype     : "diagnostic",
-	id        : "log",
-	className : "OMV.module.admin.diagnostic.log.plugin.Cups"
+    id       : "cupsaccess",
+    text     : _("Printing Access Log"),
+    stateful : true,
+    stateId  : "053eba40-6374-11e3-949a-0800200c9a66",
+    columns  : [{
+        text      : _("Date & Time"),
+        sortable  : true,
+        dataIndex : "date",
+        stateId   : "date",
+        renderer  : OMV.util.Format.localeTimeRenderer()
+    },{
+        text      : _("Host"),
+        sortable  : true,
+        dataIndex : "host",
+        stateId   : "host",
+        flex      : 1
+    },{
+        text      : _("User"),
+        sortable  : true,
+        dataIndex : "user",
+        stateId   : "user",
+        flex      : 1
+    },{
+        text      : _("Event"),
+        sortable  : true,
+        dataIndex : "event",
+        stateId   : "event",
+        flex      : 1
+    }],
+    rpcParams : {
+        id : "cupsaccess"
+    },
+    rpcFields : [
+        { name : "date", type : "string" },
+        { name : "host", type : "string" },
+        { name : "user", type : "string" },
+        { name : "event", type : "string" }
+    ]
 });
--- var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/CupsError.js
+++ var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/CupsError.js
@@ -21,53 +21,48 @@
 // require("js/omv/util/Format.js"
 
 Ext.define("OMV.module.admin.diagnostic.log.plugin.CupsError", {
-	extend : "OMV.module.admin.diagnostic.log.plugin.Plugin",
+    extend : "OMV.module.admin.diagnostic.log.plugin.Plugin",
+    alias  : "omv.plugin.diagnostic.log.cups.access",
 
-	id      : "cupserror",
-	text    :_("Printing Error Log"),
-	stateId : "206ae781-6374-11e3-949a-0800200c9a66",
-	columns : [{
-		text      : _("Date & Time"),
-		sortable  : true,
-		dataIndex : "date",
-		renderer: OMV.util.Format.localeTimeRenderer()
-	},{
-		text      : _("Severity"),
-		sortable  : true,
-		dataIndex : "severity",
-		flex      : 1,
-		renderer  : function (value) {
-			switch (value) {
-				case "E":
-					return _("Error");
-				case "W":
-					return _("Warning");
-				case "I":
-					return _("Info");
-				case "D":
-					return _("Debug");
-			}
+    id      : "cupserror",
+    text    :_("Printing Error Log"),
+    stateId : "206ae781-6374-11e3-949a-0800200c9a66",
+    columns : [{
+        text      : _("Date & Time"),
+        sortable  : true,
+        dataIndex : "date",
+        renderer: OMV.util.Format.localeTimeRenderer()
+    },{
+        text      : _("Severity"),
+        sortable  : true,
+        dataIndex : "severity",
+        flex      : 1,
+        renderer  : function (value) {
+            switch (value) {
+                case "E":
+                    return _("Error");
+                case "W":
+                    return _("Warning");
+                case "I":
+                    return _("Info");
+                case "D":
+                    return _("Debug");
+            }
 
-			return value;
-		}
-	},{
-		header    : _("Event"),
-		sortable  : true,
-		dataIndex : "event",
-		flex      : 1
-	}],
-	rpcParams: {
-		id: "cupserror"
-	},
-	rpcFields: [
-		{ name : "date", type : "string" },
-		{ name : "severity", type : "string" },
-		{ name : "event", type : "string" }
-	]
-});
-
-OMV.PluginManager.register({
-	ptype     : "diagnostic",
-	id        : "log",
-	className : "OMV.module.admin.diagnostic.log.plugin.CupsError"
+            return value;
+        }
+    },{
+        header    : _("Event"),
+        sortable  : true,
+        dataIndex : "event",
+        flex      : 1
+    }],
+    rpcParams: {
+        id: "cupserror"
+    },
+    rpcFields: [
+        { name : "date", type : "string" },
+        { name : "severity", type : "string" },
+        { name : "event", type : "string" }
+    ]
 });
